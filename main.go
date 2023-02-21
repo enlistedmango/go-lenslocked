7ff45fd75032b9289bb4aaeaaf72dd663835cbd4
enlistedmango <jamiemccallum0@gmail.com>
2023-02-21 17:11:41 +0000

    Updates

--- main.go
+++ main.go
@@ -15,8 +15,12 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:mango@notmango.dev\">mango@notmango.dev</a>")
 }
 
+func pathhandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", pathhandler)
 	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
