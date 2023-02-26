6b590e7d766ecc5279d6ed211c8bb9f5719acc51
enlistedmango <jamiemccallum0@gmail.com>
2023-02-26 20:50:11 +0000

    File updates

--- main.go
+++ main.go
@@ -15,13 +15,31 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:mango@notmango.dev\">mango@notmango.dev</a>")
 }
 
-func pathhandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, r.URL.Path)
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	}
 }
 
+// type Router struct{}
+
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page Not Found", http.StatusNotFound)
+// 	}
+// }
+
 func main() {
-	http.HandleFunc("/", pathhandler)
-	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 }
