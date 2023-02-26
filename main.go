958708225eb503b21d506177ddc7b6c31d9e9d81
enlistedmango <jamiemccallum0@gmail.com>
2023-02-26 21:46:14 +0000

    Completed FAQ exercise page.

--- main.go
+++ main.go
@@ -15,31 +15,46 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:mango@notmango.dev\">mango@notmango.dev</a>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w,
+		"<h1>FAQs</h1>"+
+			"<p>Q: Is there a free version?<p>"+
+			"<p>A: Yes! We offer a free trial for 30 days on any paid plans.</p>"+
+			"<p>Q: What are your support hours?</p>"+
+			"<p>A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</p?"+
+			"<p>Q: How do I contact support?</p>"+
+			"<p>A: Email us - <a href=\"mailto:support@lenslocked.com\">Support@lenslocked.com</a>")
+}
+
+//func pathHandler(w http.ResponseWriter, r *http.Request) {
+//	switch r.URL.Path {
+//	case "/":
+//		homeHandler(w, r)
+//	case "/contact":
+//		contactHandler(w, r)
+//	default:
+//		http.Error(w, "Page Not Found", http.StatusNotFound)
+//	}
+//}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	}
 }
 
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page Not Found", http.StatusNotFound)
-// 	}
-// }
-
 func main() {
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", router)
 }
