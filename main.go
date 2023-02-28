a9ea694ce489fb221c5b2696afbfc7aa7360dec6
Mango <82413197+enlistedmango@users.noreply.github.com>
2023-02-28 09:37:15 +0000

    pushing random changes

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"net/http"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
