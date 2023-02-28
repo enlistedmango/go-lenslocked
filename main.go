7ee7cac453c05ac899a2228bfbfc42ee9229e7e2
Mango <82413197+enlistedmango@users.noreply.github.com>
2023-02-28 21:38:43 +0000

    Refactored the homeHandler to use an html template to render

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,7 +12,18 @@ import (
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	tpl, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil { // This error is if templates don't parse correctly
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return // The return statement is important because it stops the code from progressing.
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil { // This error is if we can't execute for whichever reason
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executig the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
